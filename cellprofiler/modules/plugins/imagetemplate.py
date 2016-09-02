87a68a1a59a2025432f584aa33640ccdf3643bc3
mcquin <mcquin@broadinstitute.org>
2016-09-02 14:26:37 -0400

    fix formatting

--- cellprofiler/modules/plugins/imagetemplate.py
+++ cellprofiler/modules/plugins/imagetemplate.py
@@ -42,7 +42,6 @@ GRADIENT_DIRECTION_Y = "Gradient direction - Y"
 # implement only what you need.
 #
 ###################################
-
 class ImageTemplate(cellprofiler.module.Module):
     ###############################################
     #
@@ -65,7 +64,6 @@ class ImageTemplate(cellprofiler.module.Module):
     # settings you can use.
     #
     ################################################
-
     def create_settings(self):
         #
         # The ImageNameSubscriber "subscribes" to all ImageNameProviders in
@@ -74,44 +72,48 @@ class ImageTemplate(cellprofiler.module.Module):
         # which can then be used as inputs in your module.
         #
         self.input_image_name = cellprofiler.setting.ImageNameSubscriber(
-                # The text to the left of the edit box
-                "Input image name:",
-                # HTML help that gets displayed when the user presses the
-                # help button to the right of the edit box
-                doc="""This is the image that the module operates on. You can
+            # The text to the left of the edit box
+            "Input image name:",
+            # HTML help that gets displayed when the user presses the
+            # help button to the right of the edit box
+            doc="""This is the image that the module operates on. You can
             choose any image that is made available by a prior module.
             <br>
             <b>ImageTemplate</b> will do something to this image.
-            """)
+            """
+        )
+
         #
         # The ImageNameProvider makes the image available to subsequent
         # modules.
         #
         self.output_image_name = cellprofiler.setting.ImageNameProvider(
-                "Output image name:",
-                # The second parameter holds a suggested name for the image.
-                "OutputImage",
-                doc="""This is the image resulting from the operation.""")
+            "Output image name:",
+            # The second parameter holds a suggested name for the image.
+            "OutputImage",
+            doc="""This is the image resulting from the operation."""
+        )
+
         #
         # Here's a choice box - the user gets a drop-down list of what
         # can be done.
         #
         self.gradient_choice = cellprofiler.setting.Choice(
-                "Gradient choice:",
-                # The choice takes a list of possibilities. The first one
-                # is the default - the one the user will typically choose.
-                [GRADIENT_MAGNITUDE, GRADIENT_DIRECTION_X, GRADIENT_DIRECTION_Y],
-                #
-                # Here, in the documentation, we do a little trick so that
-                # we use the actual text that's displayed in the documentation.
-                #
-                # %(GRADIENT_MAGNITUDE)s will get changed into "Gradient magnitude"
-                # etc. Python will look in globals() for the "GRADIENT_" names
-                # and paste them in where it sees %(GRADIENT_...)s
-                #
-                # The <ul> and <li> tags make a neat bullet-point list in the docs
-                #
-                doc="""Choose what to calculate:
+            "Gradient choice:",
+            # The choice takes a list of possibilities. The first one
+            # is the default - the one the user will typically choose.
+            [GRADIENT_MAGNITUDE, GRADIENT_DIRECTION_X, GRADIENT_DIRECTION_Y],
+            #
+            # Here, in the documentation, we do a little trick so that
+            # we use the actual text that's displayed in the documentation.
+            #
+            # %(GRADIENT_MAGNITUDE)s will get changed into "Gradient magnitude"
+            # etc. Python will look in globals() for the "GRADIENT_" names
+            # and paste them in where it sees %(GRADIENT_...)s
+            #
+            # The <ul> and <li> tags make a neat bullet-point list in the docs
+            #
+            doc="""Choose what to calculate:
             <ul>
             <li><i>%(GRADIENT_MAGNITUDE)s</i> to calculate the
             magnitude of the gradient at each pixel.</li>
@@ -122,17 +124,19 @@ class ImageTemplate(cellprofiler.module.Module):
             <li><i>%(GRADIENT_DIRECTION_Y)s</i> to get the relative
             contribution of the gradient in the Y direction.</li></ul>
             """ % globals()
-
         )
+
         #
         # A binary setting displays a checkbox.
         #
         self.automatic_smoothing = cellprofiler.setting.Binary(
-                "Automatically choose the smoothing scale?",
-                # The default value is to choose automatically
-                True,
-                doc="""The module will automatically choose a
-            smoothing scale for you if you leave this checked.""")
+            "Automatically choose the smoothing scale?",
+            # The default value is to choose automatically
+            True,
+            doc="""The module will automatically choose a
+            smoothing scale for you if you leave this checked."""
+        )
+
         #
         # We do a little smoothing which supplies a scale to the gradient.
         #
@@ -141,18 +145,19 @@ class ImageTemplate(cellprofiler.module.Module):
         # an invalid scale.
         #
         self.scale = cellprofiler.setting.Float(
-                "Scale:",
-                # The default value is 1 - a short-range scale
-                1,
-                # We don't let the user type in really small values
-                minval=.1,
-                # or large values
-                maxval=100,
-                doc="""This is a scaling factor that supplies the sigma for
+            "Scale:",
+            # The default value is 1 - a short-range scale
+            1,
+            # We don't let the user type in really small values
+            minval=.1,
+            # or large values
+            maxval=100,
+            doc="""This is a scaling factor that supplies the sigma for
             a gaussian that's used to smooth the image. The gradient is
             calculated on the smoothed image, so large scales will give
             you long-range gradients and small scales will give you
-            short-range gradients""")
+            short-range gradients"""
+        )
 
     #
     # The "settings" method tells CellProfiler about the settings you
@@ -161,9 +166,13 @@ class ImageTemplate(cellprofiler.module.Module):
     # pipeline file.
     #
     def settings(self):
-        return [self.input_image_name, self.output_image_name,
-                self.gradient_choice, self.automatic_smoothing,
-                self.scale]
+        return [
+            self.input_image_name,
+            self.output_image_name,
+            self.gradient_choice,
+            self.automatic_smoothing,
+            self.scale
+        ]
 
     #
     # visible_settings tells CellProfiler which settings should be
@@ -174,13 +183,19 @@ class ImageTemplate(cellprofiler.module.Module):
     # for display.
     #
     def visible_settings(self):
-        result = [self.input_image_name, self.output_image_name,
-                  self.gradient_choice, self.automatic_smoothing]
+        result = [
+            self.input_image_name,
+            self.output_image_name,
+            self.gradient_choice,
+            self.automatic_smoothing
+        ]
+
         #
         # Show the user the scale only if self.wants_smoothing is checked
         #
         if not self.automatic_smoothing:
             result += [self.scale]
+
         return result
 
     #
@@ -194,22 +209,26 @@ class ImageTemplate(cellprofiler.module.Module):
         # the string name.
         #
         input_image_name = self.input_image_name.value
+
         output_image_name = self.output_image_name.value
+
         #
         # Get the image set. The image set has all of the images in it.
         #
         image_set = workspace.image_set
+
         #
         # Get the input image object. We want a grayscale image here.
         # The image set will convert a color image to a grayscale one
         # and warn the user.
         #
-        input_image = image_set.get_image(input_image_name,
-                                          must_be_grayscale=True)
+        input_image = image_set.get_image(input_image_name, must_be_grayscale=True)
+
         #
         # Get the pixels - these are a 2-d Numpy array.
         #
         pixels = input_image.pixel_data
+
         #
         # Get the smoothing parameter
         #
@@ -218,37 +237,50 @@ class ImageTemplate(cellprofiler.module.Module):
             # is pretty hokey, not intended to really find a good number.
             #
             fft = numpy.fft.fft2(pixels)
+
             power2 = numpy.sqrt((fft * fft.conjugate()).real)
+
             mode = numpy.argwhere(power2 == power2.max())[0]
+
             scale = numpy.sqrt(numpy.sum((mode + .5) ** 2))
         else:
             scale = self.scale.value
-        g = scipy.ndimage.gaussian_gradient_magnitude(pixels, scale)
+
+        gradient = scipy.ndimage.gaussian_gradient_magnitude(pixels, scale)
+
         if self.gradient_choice == GRADIENT_MAGNITUDE:
-            output_pixels = g
+            output_pixels = gradient
         else:
             # Numpy uses i and j instead of x and y. The x axis is 1
             # and the y axis is 0
-            x = scipy.ndimage.correlate1d(g, [-1, 0, 1], 1)
-            y = scipy.ndimage.correlate1d(g, [-1, 0, 1], 0)
+            x = scipy.ndimage.correlate1d(gradient, [-1, 0, 1], 1)
+
+            y = scipy.ndimage.correlate1d(gradient, [-1, 0, 1], 0)
+
             norm = numpy.sqrt(x ** 2 + y ** 2)
+
             if self.gradient_choice == GRADIENT_DIRECTION_X:
                 output_pixels = .5 + x / norm / 2
             else:
                 output_pixels = .5 + y / norm / 2
+
         #
         # Make an image object. It's nice if you tell CellProfiler
         # about the parent image - the child inherits the parent's
         # cropping and masking, but it's not absolutely necessary
         #
         output_image = cellprofiler.image.Image(output_pixels, parent_image=input_image)
+
         image_set.add(output_image_name, output_image)
+
         #
         # Save intermediate results for display if the window frame is on
         #
         if self.show_window:
             workspace.display_data.input_pixels = pixels
-            workspace.display_data.gradient = g
+
+            workspace.display_data.gradient = gradient
+
             workspace.display_data.output_pixels = output_pixels
 
     #
@@ -261,28 +293,40 @@ class ImageTemplate(cellprofiler.module.Module):
         # so we pretty much ignore the figure.
         #
         figure = workspace.create_or_find_figure(subplots=(3, 1))
+
         #
         # Show the user the input image
         #
         figure.subplot_imshow_grayscale(
-                0, 0,  # show the image in the first row and column
-                workspace.display_data.input_pixels,
-                title=self.input_image_name.value)
+            0,
+            0,
+            workspace.display_data.input_pixels,
+            title=self.input_image_name.value
+        )
+
         lead_subplot = figure.subplot(0, 0)
+
         #
         # Show the user the gradient image, linking it to the first
         # so that they zoom and pan together
         #
         figure.subplot_imshow_grayscale(
-                1, 0,  # show the image in the first row and second column
-                workspace.display_data.gradient,
-                title="Gradient",
-                sharex=lead_subplot, sharey=lead_subplot)
+            1,
+            0,
+            workspace.display_data.gradient,
+            title="Gradient",
+            sharex=lead_subplot,
+            sharey=lead_subplot
+        )
+
         #
         # Show the user the final image
         #
         figure.subplot_imshow_grayscale(
-                2, 0,  # show the image in the first row and last column
-                workspace.display_data.output_pixels,
-                title=self.output_image_name.value,
-                sharex=lead_subplot, sharey=lead_subplot)
+            2,
+            0,
+            workspace.display_data.output_pixels,
+            title=self.output_image_name.value,
+            sharex=lead_subplot,
+            sharey=lead_subplot
+        )
