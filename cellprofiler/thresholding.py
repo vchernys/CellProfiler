ad9878d1941d0d4e22907c3a7bdb1e2430995b7b
mcquin <mcquin@broadinstitute.org>
2017-02-12 13:45:19 -0500

    move thresholding operations to own module

---
+++ cellprofiler/thresholding.py
@@ -0,0 +1,80 @@
+import centrosome.otsu
+import numpy
+import skimage.filters
+import skimage.filters.rank
+import skimage.morphology
+
+
+def otsu(image):
+    data = image.pixel_data
+
+    mask = image.mask
+
+    if numpy.all(data[mask] == data[mask][0]):
+        return data[mask][0]
+
+    return skimage.filters.threshold_otsu(data[mask])
+
+
+def otsu3(image):
+    return centrosome.otsu.otsu3(image.pixel_data[image.mask])
+
+
+def local_otsu(image, block_size):
+    data = skimage.img_as_ubyte(image.pixel_data)
+
+    selem = skimage.morphology.square(block_size)
+
+    if image.volumetric:
+        threshold = numpy.zeros_like(data)
+
+        for index, plane in enumerate(data):
+            threshold[index] = skimage.filters.rank.otsu(plane, selem, mask=image.mask[index])
+    else:
+        threshold = skimage.filters.rank.otsu(data, selem, mask=image.mask)
+
+    return skimage.img_as_float(threshold)
+
+
+def local_otsu3(image, block_size):
+    assert block_size % 2 == 1, "block_size must be odd, got {}".format(block_size)
+
+    data = image.pixel_data
+
+    data[~image.mask] = 0
+
+    if image.volumetric:
+        lower = numpy.zeros_like(data)
+
+        upper = numpy.zeros_like(data)
+
+        for index, plane in enumerate(data):
+            lower = skimage.filters.threshold_local(
+                plane,
+                block_size,
+                method="generic",
+                param=lambda x: centrosome.otsu.otsu3(x)[0]
+            )
+
+            upper = skimage.filters.threshold_local(
+                plane,
+                block_size,
+                method="generic",
+                param=lambda x: centrosome.otsu.otsu3(x)[1]
+            )
+    else:
+        lower = skimage.filters.threshold_local(
+            data,
+            block_size,
+            method="generic",
+            param=lambda x: centrosome.otsu.otsu3(x)[0]
+        )
+
+        upper = skimage.filters.threshold_local(
+            data,
+            block_size,
+            method="generic",
+            param=lambda x: centrosome.otsu.otsu3(x)[1]
+        )
+
+    return lower, upper
---
+++ tests/test_thresholding.py
@@ -0,0 +1,233 @@
+import cellprofiler.image
+import cellprofiler.thresholding
+import numpy
+import numpy.random
+import pytest
+
+
+numpy.random.seed(73)
+
+
+def test_otsu_zeros():
+    image = cellprofiler.image.Image(numpy.zeros((10, 10), dtype=numpy.float32))
+
+    threshold = cellprofiler.thresholding.otsu(image)
+
+    assert threshold == 0.0
+
+
+def test_otsu_masked_zeros():
+    data = numpy.random.rand(10, 10)
+
+    data[2:5, 2:5] = 0.0
+
+    mask = numpy.zeros_like(data, dtype=numpy.bool)
+
+    mask[2:5, 2:5] = True
+
+    image = cellprofiler.image.Image(data, mask=mask)
+
+    threshold = cellprofiler.thresholding.otsu(image)
+
+    assert threshold == 0.0
+
+
+def test_otsu_image():
+    data = numpy.random.rand(10, 10)
+
+    image = cellprofiler.image.Image(data)
+
+    threshold = cellprofiler.thresholding.otsu(image)
+
+    assert threshold >= 0.0
+
+    assert threshold <= 1.0
+
+
+def test_otsu_volume():
+    data = numpy.random.rand(10, 10, 10)
+
+    image = cellprofiler.image.Image(data, dimensions=3)
+
+    threshold = cellprofiler.thresholding.otsu(image)
+
+    assert threshold >= 0.0
+
+    assert threshold <= 1.0
+
+
+def test_local_otsu_image_zeros():
+    image = cellprofiler.image.Image(numpy.zeros((10, 10), dtype=numpy.float32))
+
+    threshold = cellprofiler.thresholding.local_otsu(image, 3)
+
+    assert numpy.all(threshold == 0.0)
+
+
+def test_local_otsu_masked_image_zeros():
+    data = numpy.random.rand(10, 10)
+
+    data[2:5, 2:5] = 0.0
+
+    mask = numpy.zeros_like(data, dtype=numpy.bool)
+
+    mask[2:5, 2:5] = True
+
+    image = cellprofiler.image.Image(data, mask=mask)
+
+    threshold = cellprofiler.thresholding.local_otsu(image, 3)
+
+    assert numpy.all(threshold == 0.0)
+
+
+def test_local_otsu_image():
+    data = numpy.random.rand(10, 10)
+
+    image = cellprofiler.image.Image(data)
+
+    threshold = cellprofiler.thresholding.local_otsu(image, 3)
+
+    assert numpy.all(threshold >= 0.0)
+
+    assert numpy.all(threshold <= 1.0)
+
+
+def test_local_otsu_volume():
+    data = numpy.random.rand(10, 10, 10)
+
+    image = cellprofiler.image.Image(data, dimensions=3)
+
+    threshold = cellprofiler.thresholding.local_otsu(image, 3)
+
+    assert numpy.all(threshold >= 0.0)
+
+    assert numpy.all(threshold <= 1.0)
+
+
+def test_otsu3_zeros():
+    image = cellprofiler.image.Image(numpy.zeros((10, 10), dtype=numpy.float32))
+
+    lower, upper = cellprofiler.thresholding.otsu3(image)
+
+    assert lower == 0.0
+
+    assert upper == 0.0
+
+
+def test_otsu3_masked_zeros():
+    data = numpy.random.rand(10, 10)
+
+    data[2:5, 2:5] = 0.0
+
+    mask = numpy.zeros_like(data, dtype=numpy.bool)
+
+    mask[2:5, 2:5] = True
+
+    image = cellprofiler.image.Image(data, mask=mask)
+
+    lower, upper = cellprofiler.thresholding.otsu3(image)
+
+    assert lower == 0.0
+
+    assert upper == 0.0
+
+
+def test_otsu3_image():
+    data = numpy.random.rand(10, 10)
+
+    image = cellprofiler.image.Image(data)
+
+    lower, upper = cellprofiler.thresholding.otsu3(image)
+
+    assert lower >= 0.0
+
+    assert upper >= 0.0
+
+    assert lower <= 1.0
+
+    assert upper <= 1.0
+
+
+def test_otsu3_volume():
+    data = numpy.random.rand(10, 10, 10)
+
+    image = cellprofiler.image.Image(data, dimensions=3)
+
+    lower, upper = cellprofiler.thresholding.otsu3(image)
+
+    assert lower >= 0.0
+
+    assert upper >= 0.0
+
+    assert lower <= 1.0
+
+    assert upper <= 1.0
+
+
+def test_local_otsu3_even_block_size():
+    image = cellprofiler.image.Image(numpy.zeros((10, 10), dtype=numpy.float32))
+
+    with pytest.raises(AssertionError) as error:
+        cellprofiler.thresholding.local_otsu3(image, 2)
+
+    assert "block_size must be odd, got 2" in str(error.value)
+
+
+def test_local_otsu3_zeros():
+    image = cellprofiler.image.Image(numpy.zeros((10, 10), dtype=numpy.float32))
+
+    lower, upper = cellprofiler.thresholding.local_otsu3(image, 3)
+
+    assert numpy.all(lower == 0.0)
+
+    assert numpy.all(upper == 0.0)
+
+
+def test_local_otsu3_masked_zeros():
+    data = numpy.random.rand(10, 10)
+
+    data[2:5, 2:5] = 0.0
+
+    mask = numpy.zeros_like(data, dtype=numpy.bool)
+
+    mask[2:5, 2:5] = True
+
+    image = cellprofiler.image.Image(data, mask=mask)
+
+    lower, upper = cellprofiler.thresholding.local_otsu3(image, 3)
+
+    assert numpy.all(lower == 0.0)
+
+    assert numpy.all(upper == 0.0)
+
+
+def test_local_otsu3_image():
+    data = numpy.random.rand(10, 10)
+
+    image = cellprofiler.image.Image(data)
+
+    lower, upper = cellprofiler.thresholding.local_otsu3(image, 3)
+
+    assert numpy.all(lower >= 0.0)
+
+    assert numpy.all(upper >= 0.0)
+
+    assert numpy.all(lower <= 1.0)
+
+    assert numpy.all(upper <= 1.0)
+
+
+def test_local_otsu3_volume():
+    data = numpy.random.rand(10, 10, 10)
+
+    image = cellprofiler.image.Image(data, dimensions=3)
+
+    lower, upper = cellprofiler.thresholding.local_otsu3(image, 3)
+
+    assert numpy.all(lower >= 0.0)
+
+    assert numpy.all(upper >= 0.0)
+
+    assert numpy.all(lower <= 1.0)
+
+    assert numpy.all(upper <= 1.0)
