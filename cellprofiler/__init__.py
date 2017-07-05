53b76456b7b7cbbe56b9ed7b08073ebaed865a71
Allen Goodman <allen.goodman@icloud.com>
2017-07-05 15:27:32 +0000

    setup fixes

--- cellprofiler/__init__.py
+++ cellprofiler/__init__.py
@@ -3,5 +3,4 @@ import os.path
 
 __test__ = False
 
-__version_file__ = open(os.path.join(os.path.dirname(__file__), 'VERSION'))
-__version__ = __version_file__.read().strip()
+__version__ = "3.0.0"
